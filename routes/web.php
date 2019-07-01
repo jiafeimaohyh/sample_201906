e50fc150a55d438d43f525f137eec518763bf7bc
huyuhui <huyuhui@admin.com>
2019-07-01 06:18:41 +0000

    Add account activations

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,4 @@ Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
